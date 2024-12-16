61c237bb1f2b2b475a19908554091c17b32dbb6c
mrcreatist <ahk.verma102@gmail.com>
2024-12-16 14:05:41 +0530

    added changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Endpoint to get the QR code URL
 app.get('/generate-qr', (req, res) => {
-    const otpauth = otplib.authenticator.keyuri('user', 'TOTP Generator', secret);
+    const otpauth = otplib.authenticator.keyuri('user', 'TOTP', secret);
     qrcode.toDataURL(otpauth, (err, qrCodeUrl) => {
         if (err) return res.status(500).send('Error generating QR code');
         res.json({ qrCodeUrl });
